1c028fd1dd985b35a202eb81fc30b73595f506fd
DAYWIN007 <wanchai.ma@rmuti.ac.th>
2025-03-01 16:50:47 +0700

    code

--- miniproject/myapp/models.py
+++ miniproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product:
+    def __init__(self,brand,proid,images,proname,detail,price):
+        self.brand = brand
+        self.proid = proid
+        self.images = images
+        self.proname = proname
+        self.detail = detail
+        self.price = price
